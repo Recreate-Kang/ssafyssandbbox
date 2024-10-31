397159a74a706e0a58a40693531615e922046bad
강재현 <recreate.kang@gmail.com>
2024-11-01 01:59:28 +0900

    refactor: TodoService(createTodos)의 sql에서 처리되어 불필요한 연산 제거

--- sandbox/src/main/java/com/ssafy/sandbox/service/TodoService.java
+++ sandbox/src/main/java/com/ssafy/sandbox/service/TodoService.java
@@ -7,7 +7,6 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Map;
 
 @Service
@@ -16,12 +15,9 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
     public TodoListResponse getAllTodos() {
-        TodoListResponse response = new TodoListResponse(todoRepository.findAll());
-        return response;
+        return new TodoListResponse(todoRepository.findAll());
     }
     public TodoDto createTodos(TodoDto newTodoDto) {
-        newTodoDto.setCompleted(false); // 기본값 설정
-        newTodoDto.setCreatedAt(LocalDateTime.now()); // 현재 시간 설정
         newTodoDto.setUserId(1); // 사용자 ID 설정 (예시)
         return todoRepository.save(newTodoDto); // 저장 메서드 호출
     }
